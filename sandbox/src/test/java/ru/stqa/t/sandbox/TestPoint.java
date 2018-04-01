378b96dc4390d94f167b69d67e2452b08f216f47
asasimov <37741229+asasimov@users.noreply.github.com>
2018-04-01 12:23:35 +0300

    Изменен класс TestPoint

--- sandbox/src/test/java/ru/stqa/t/sandbox/TestPoint.java
+++ sandbox/src/test/java/ru/stqa/t/sandbox/TestPoint.java
@@ -9,6 +9,7 @@ public class TestPoint {
     public void testsPoint() {
         Point p1 = new Point(1,2);
         Point p2 = new Point(5,8);
-        Assert.assertEquals(p1.distance(p2), 2.8284271247462);
+        Assert.assertEquals(p1.distance(p2), 7.211102550927978);
+        Assert.assertEquals(Point.distance(p1, p2), 7.211102550927978);
     }
 }
