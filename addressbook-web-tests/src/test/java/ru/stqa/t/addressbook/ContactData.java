c80701bbf7b550e53a28c360ce4d72ad53a2b1b0
asasimov <37741229+asasimov@users.noreply.github.com>
2018-04-08 17:51:39 +0300

    Созданы классы ContactCreationTest и ContactData

---
+++ addressbook-web-tests/src/test/java/ru/stqa/t/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.stqa.t.addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String lastName;
+    private final String nickName;
+    private final String email;
+
+    public ContactData(String firstName, String lastName, String nickName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nickName = nickName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
