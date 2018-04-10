4caf17d61eb246f9657e21bccef38f88140b8ef1
asasimov <37741229+asasimov@users.noreply.github.com>
2018-04-10 12:07:47 +0300

    Изменен класс ContactCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/t/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/t/addressbook/TestBase.java
@@ -80,4 +80,28 @@ public class TestBase {
     protected void selectGroup() {
         wd.findElement(By.xpath("//div[@id='content']/form/span[3]/input")).click();
     }
+
+    protected void returnToContactPage() {
+        wd.findElement(By.linkText("home page")).click();
+    }
+
+    protected void fillContactForm(ContactData contactData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
+        wd.findElement(By.name("nickname")).click();
+        wd.findElement(By.name("nickname")).clear();
+        wd.findElement(By.name("nickname")).sendKeys(contactData.getNickName());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    protected void gotoAddNewContact() {
+        wd.findElement(By.linkText("add new")).click();
+    }
 }
