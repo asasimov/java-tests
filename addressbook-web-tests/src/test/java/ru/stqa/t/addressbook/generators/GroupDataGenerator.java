a3719b9b8cf277cb6ce175e9d721ce3c2c4ddcba
asasimov <37741229+asasimov@users.noreply.github.com>
2018-05-06 01:36:20 +0300

    Добавлена библиотека Gson для сериализации объектов в json

--- addressbook-web-tests/src/test/java/ru/stqa/t/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/t/addressbook/generators/GroupDataGenerator.java
@@ -3,9 +3,9 @@ package ru.stqa.t.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.xml.DomDriver;
-import com.thoughtworks.xstream.io.xml.StaxDriver;
 import ru.stqa.t.addressbook.model.GroupData;
 
 import java.io.File;
@@ -46,9 +46,19 @@ public class GroupDataGenerator {
             saveAsCsv(groups, new File(file));
         } else if (format.equals("xml")) {
             saveAsXml(groups, new File(file));
+        } else if (format.equals("json")) {
+            saveAsJson(groups, new File(file));
         } else {
             System.out.println("Unrecognized format" + format);
         }
+    }
+
+    private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+        String json = gson.toJson(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(json);
+        writer.close();
     }
 
     private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
