90b991228a03dc3af76bc00288e5dd8b0d2eff7a
asasimov <37741229+asasimov@users.noreply.github.com>
2018-05-05 18:28:58 +0300

    Параметризирован тестовый метод testGroupCreation

--- addressbook-web-tests/src/test/java/ru/stqa/t/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/t/addressbook/tests/GroupCreationTests.java
@@ -1,19 +1,37 @@
 package ru.stqa.t.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.t.addressbook.model.GroupData;
 import ru.stqa.t.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
 public class GroupCreationTests extends TestBase {
 
-    @Test
-    public void testGroupCreation() {
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        /*list.add(new Object[] {"test1", "header 1", "footer 1"});
+        list.add(new Object[] {"test2", "header 2", "footer 2"});
+        list.add(new Object[] {"test3", "header 3", "footer 3"});*/
+        list.add(new Object[] { new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
+        list.add(new Object[] { new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
+        list.add(new Object[] { new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
 
         app.goTo().groupPage();
-        GroupData group = new GroupData().withName("testGroupName_1");
+        /*GroupData group = new GroupData().withName(name)
+                .withHeader(header).withFooter(footer);*/
         Groups before = app.group().all();
 
         app.group().create(group);
