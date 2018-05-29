fbad7a4167c527444c8a3d9101fe26ac57238d76
asasimov <37741229+asasimov@users.noreply.github.com>
2018-05-29 21:57:14 +0300

    Добавлена библиотека jcabi-github для интеграции с Github через API

---
+++ github-sample/src/test/java/ru/stqa/t/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.t.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub("687654aba4e3035c793c8c580062309c8b2c6444");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("asasimov", "java_t")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+}
